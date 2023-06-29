bd72e9d3fb35ee693c363a439cacf4127701840a
Agzhan Batyrbek <agjanbatyrbek855@gmail.com>
2023-06-29 16:39:03 +0800

    add the button and header-text

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="max-w-[1150px] mx-auto mt-24 text-white">
+      <h1 className="text-6xl uppercase bold">we help you to make</h1>
+      <h1 className="text-6xl uppercase bold">things happen</h1>
+
+      <button className="contactUs-b uppercase text-3xl bg-white text-black p-3 rounded-2xl mt-32 bold ">
+        contact us now
+      </button>
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/images/logo.png";
+import "./navbar.css";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   return (
-    <div className="flex text-white justify-end items-center h-24 max-w-[1080px] px-4 mx-auto relative">
-      <div className=" w-auto bg-white p-4 py-5 rounded-br-2xl rounded-bl-2xl absolute left-0 top-0 items-center">
-        <img
-          className="max-with-full h-auto"
-          src={logo}
-          alt=""
-          style={{ width: "8vw" }}
-        />
+    <div className="flex text-white justify-end items-center h-24 lg:max-w-[900px] xl:max-w-[1200px]  px-4 mx-auto relative md:max-w-[710px] max-w-[450px]">
+      <div className="absolute left-2/4 top-0  lg:left-0 lg:top-0">
+        <div className=" -left-1/2 relative w-auto bg-white p-4 py-5 rounded-br-2xl rounded-bl-2xl abolute lg:left-0 lg:top-0 items-center">
+          <img className="lg:w-32 md:w-28 w-20" src={logo} alt="" />
+        </div>
       </div>
-      <ul className="flex text-lg">
+      <ul className="text-lg hidden lg:flex">
         <li className="p-4">WHAT DO WE DO</li>
         <li className="p-4">OUR PROJECTS</li>
         <li className="p-4">OUR TEAM</li>
         <li className="p-4">REVIEWS</li>
         <li className="p-4"> EN </li>
       </ul>
+      <div onClick={handleNav} className="block lg:hidden">
+        {!nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={50} />}
+        {/* <AiOutlineMenu size={50} /> */}
+      </div>
+
+      <div
+        className={
+          !nav
+            ? "fixed w-full md:w-5/6 text-center left-0 top-0  border-r-gray-900 mt-24 md:mt-32 py-0  bg-black my-auto centerFixed ease-in-out duration-500"
+            : "mt-24 md:mt-32 fixed left-[-100%]"
+        }
+      >
+        <ul className="">
+          <li className="p-4 border-b">WHAT DO WE DO</li>
+          <li className="p-4 border-b">OUR PROJECTS</li>
+          <li className="p-4 border-b">OUR TEAM</li>
+          <li className="p-4 border-b">REVIEWS</li>
+          <li className="p-4"> EN </li>
+        </ul>
+      </div>
     </div>
   );
 };
